9797af0ee24f88036567a0b9cabaf32ae3ff4111
Chetan Gowda <chetan.hs@gmail.com>
2013-07-28 17:52:19 -0700

    [GoUtils] added more types to Pprint()

--- debug/pprint.go
+++ debug/pprint.go
@@ -1,39 +1,78 @@
 package debug
 
-import(
-"fmt"
-"reflect"
-"strings"
+import (
+	"fmt"
+	"reflect"
+	"strings"
 )
 
-const(
-INDENT_WIDTH = 2
+const (
+	INDENT_WIDTH = 2
 )
 
 func Pprint(obj interface{}) string {
-  return recurPretty(obj, 1)
+	refObj := reflect.ValueOf(obj)
+	return stringify(refObj, 1, true)
 }
 
-func recurPretty(obj interface{}, depth int) string {
-  if obj == nil {
-    return ""
-  }
-  refObj := reflect.ValueOf(obj)
-  typeObj := refObj.Type()
+func stringify(refVal reflect.Value, depth int, printType bool) string {
+	switch refVal.Kind() {
+	case reflect.Bool:
+		return fmt.Sprintf("%#v", refVal.Bool())
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32,
+		reflect.Int64:
+		return fmt.Sprintf("%#v", refVal.Int())
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32,
+		reflect.Uint64:
+		return fmt.Sprintf("%#v", refVal.Uint())
+	case reflect.Ptr:
+		return stringifyPointer(refVal)
+	case reflect.Float32, reflect.Float64:
+		return fmt.Sprintf("%#v", refVal.Float())
+	case reflect.Complex64, reflect.Complex128:
+		return fmt.Sprintf("%#v", refVal.Complex())
 
-  spaces := strings.Repeat(" ", depth * INDENT_WIDTH)
-  openBraces := fmt.Sprintf("%s {\n", typeObj)
-  innerContent := ""
-  for i:=0; i<refObj.NumField(); i++ {
-    f := refObj.Field(i)
-    switch f.Kind() {
-      case reflect.Struct:
-        innerContent += spaces + recurPretty(f.Interface(), depth+1)
-      default:
-        innerContent += spaces + fmt.Sprintf("%s: %v\n", typeObj.Field(i).Name, f.Int())
-    }
-  }
-  closingBraces := fmt.Sprintln("}\n")
-  return openBraces + innerContent + closingBraces
+	case reflect.Array, reflect.Slice:
+		return stringifyList(refVal, depth)
+
+	case reflect.Struct:
+		return stringifyStruct(refVal, depth, printType)
+	default:
+		return "UNKNOWN_VALUE(" + refVal.Type().String() + ")"
+	}
+	return "<nil>"
 }
 
+func stringifyPointer(refVal reflect.Value) string {
+	return fmt.Sprintf("%#x", refVal.Pointer()) +
+		"(" + refVal.Type().String() + ")"
+}
+
+func stringifyList(refVal reflect.Value, depth int) string {
+	var strContent string
+	for i := 0; i < refVal.Len(); i++ {
+		strContent += stringify(refVal.Index(i), depth, false)
+		if i < (refVal.Len() - 1) {
+			strContent += ", "
+		}
+	}
+	return refVal.Type().String() + "{" + strContent + "}"
+}
+
+func stringifyStruct(refVal reflect.Value, depth int, printType bool) string {
+	spaces := strings.Repeat(" ", depth*INDENT_WIDTH)
+	openBraces := ""
+	if printType {
+		openBraces = fmt.Sprintf("%s {\n", refVal.Type().String())
+	} else {
+		openBraces = "{\n"
+	}
+	innerContent := ""
+	for i := 0; i < refVal.NumField(); i++ {
+		f := refVal.Field(i)
+		innerContent += spaces +
+			fmt.Sprintf("%s: %s\n", refVal.Type().Field(i).Name, stringify(f, depth+1, true))
+	}
+	closingBraces := fmt.Sprintln("}")
+	return openBraces + innerContent + spaces + closingBraces
+}
--- debug/pprint_test.go
+++ debug/pprint_test.go
@@ -1,24 +1,59 @@
 package debug
 
-import(
-"testing"
-"fmt"
+import (
+	"fmt"
+	"testing"
 )
 
 func TestPPrint(t *testing.T) {
-  type input struct {
-    //anInterface interface{}
-    //aMap map[string]string
-    //aSlice []int
-    anInt int
-  }
-  var obj = input {
-    //map[string]string{"foo": "bar", "qwerty": "qqq123"},
-    //[]int{3,6,9},
-    32,
-  }
-  res := Pprint(obj)
-  fmt.Println("Result: ", res)
-  t.Errorf("unexpected error: %s", res)
+	var someBool bool = true
+	var dummyUint32 uint32 = 3232
+	var dummyFloat64 float64 = 3.14
+	type dummyStruct struct {
+		someInt   int
+		someFloat float32
+	}
+	type input struct {
+		boolVal      bool
+		intVal       int
+		int32Val     int32
+		uintVal      uint
+		uint64Val    uint64
+		boolValPtr   *bool
+		structValPtr *dummyStruct
+		uintValPtr   *uint32
+		floatValPtr  *float64
+		floatVal     float32
+		complexVal   complex128
+		arrayVal     [8]int
+		sliceVal     []dummyStruct
+		//anInterface interface{}
+		//aMap map[string]string
+		//aSlice []int
+		//aBool bool
+		//anInt64 int64
+	}
+	var obj = input{
+		false,
+		-1,
+		32,
+		4,
+		1234567890,
+		&someBool,
+		nil,
+		&dummyUint32,
+		&dummyFloat64,
+		3.14,
+		complex128(2 + 3i),
+		[8]int{0, 1, 2, 3, 4, 5, 6, 7},
+		[]dummyStruct{
+			dummyStruct{1, 1.1},
+			dummyStruct{2, 2.2},
+		},
+		//map[string]string{"foo": "bar", "qwerty": "qqq123"},
+		//[]int{3, 6, 9},
+	}
+	res := Pprint(obj)
+	fmt.Println(res)
+	t.Errorf("unexpected error: %s", res)
 }
-
